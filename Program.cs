5273096f94fc48b783d73c75482beabaeaa15b8d
Tsig <74056836+TsiG-404@users.noreply.github.com>
2025-08-13 20:08:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineTextEditor.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("DocumentsDb"));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Documents}/{action=Index}/{id?}");
+
+app.Run();
